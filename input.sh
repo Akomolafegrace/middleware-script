fc388317bf441d29ba1c6d00528bd2db711fc5f4
root <root@ip-172-26-10-102.ec2.internal>
2024-09-21 04:03:58 +0000

    agent commit

---
+++ input.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+read -p "what is your name ? " NAME
+read -p "Are you taking the class with utrains? " c
+
+
+if [ $c = yes ]
+ then
+  echo "good job $NAME !! keep enjoying and change your life"
+else
+	echo "thats not good $NAME please check the website immediately and enroll (utrains.org)"
+
+
+fi
+
+
+
+
+
+
+
